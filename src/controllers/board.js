9eef60c78511ce5572bdb0fa63e58077e09f3577
supertree-emjay <emjay@supertree.co>
2022-10-30 11:13:07 +0900

    feat: add board CRUD

--- src/controllers/board.js
+++ src/controllers/board.js
@@ -13,13 +13,103 @@ export const postBoard = async (req, res) => {
     await data.save();
 
     res.status(200).json({
-      code: "0000",
+      success: true,
       message: null,
       data,
     });
   } catch {
     res.status(200).json({
-      code: "1000",
+      success: false,
+      message: "Error",
+      data: null,
+    });
+  }
+};
+
+export const getBoardList = async (req, res) => {
+  try {
+    const { page, limit } = req.body;
+
+    const list = await BoardSchema.find()
+      .sort({ created_at: -1 })
+      .skip((page - 1) * limit)
+      .limit(limit);
+
+    res.status(200).json({
+      success: true,
+      message: null,
+      data: list,
+    });
+  } catch {
+    res.status(200).json({
+      success: false,
+      message: "Error",
+      data: null,
+    });
+  }
+};
+
+export const getBoard = async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    const data = await BoardSchema.findById({ _id: id });
+
+    res.status(200).json({
+      success: true,
+      message: null,
+      data,
+    });
+  } catch {
+    res.status(200).json({
+      success: false,
+      message: "Error",
+      data: null,
+    });
+  }
+};
+
+export const deleteBoard = async (req, res) => {
+  try {
+    const { id } = req.body;
+
+    await BoardSchema.findByIdAndDelete({ _id: id });
+
+    res.status(200).json({
+      success: true,
+      message: null,
+      data: null,
+    });
+  } catch {
+    res.status(200).json({
+      success: false,
+      message: "Error",
+      data: null,
+    });
+  }
+};
+
+export const patchBoard = async (req, res) => {
+  try {
+    const { id, writer, title, content } = req.body;
+
+    const data = await BoardSchema.findByIdAndUpdate(
+      { _id: id },
+      {
+        writer,
+        title,
+        content,
+      }
+    );
+
+    res.status(200).json({
+      success: true,
+      message: null,
+      data,
+    });
+  } catch {
+    res.status(200).json({
+      success: false,
       message: "Error",
       data: null,
     });
--- src/routers/board.js
+++ src/routers/board.js
@@ -1,8 +1,18 @@
 import express from "express";
-import { postBoard } from "../controllers/board";
+import {
+  postBoard,
+  getBoard,
+  getBoardList,
+  deleteBoard,
+  patchBoard,
+} from "../controllers/board";
 
 const router = express.Router();
 
 router.post("/", postBoard);
+router.get("/list", getBoardList);
+router.get("/:id", getBoard);
+router.delete("/", deleteBoard);
+router.patch("/", patchBoard);
 
 export default router;
