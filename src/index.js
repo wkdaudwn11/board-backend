93ce36062b19866ff9154f2ee5438f9e09cccfab
supertree-emjay <emjay@supertree.co>
2022-11-03 18:14:23 +0900

    feat: add cors

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import morgan from "morgan";
 import helmet from "helmet";
 import bodyParser from "body-parser";
 import mongoose from "mongoose";
+import cors from "cors";
 
 import boardRouter from "./routers/board";
 import userRouter from "./routers/user";
@@ -25,6 +26,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(helmet());
 app.use(morgan("dev"));
+app.use(cors());
 
 app.use("/board", boardRouter);
 app.use("/user", userRouter);
